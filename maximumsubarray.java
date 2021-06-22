12ff0500f5a492f9a7f9ed43f059674bf8688d77
mohd shahbaz mirza <officialshahbaz98@gmail.com>
2021-06-22 23:29:22 +0530

    maximum subarray leetcode array

---
+++ maximumsubarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum= Integer.MIN_VALUE;
+        int currentSum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            currentSum+=nums[i];
+            maxSum=Math.max(maxSum,currentSum);
+            if(currentSum<0)
+                currentSum=0;
+        }
+        return maxSum;
+    }
+}
