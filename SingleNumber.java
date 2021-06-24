249c1cb716ff80c174c4f07f9fad5122c0a77cea
mohd shahbaz mirza <officialshahbaz98@gmail.com>
2021-06-25 01:46:15 +0530

    single number array leetcode easy

---
+++ SingleNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hmap.containsKey(nums[i]))
+                hmap.put(nums[i],2);
+            else
+                hmap.put(nums[i],1);
+        }
+        for(Integer in:hmap.keySet())
+        {
+            if(hmap.get(in)==1)
+                return in;
+        }
+        return nums[0];
+        
+    }
+}
