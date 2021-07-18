3367b7045d3440a88d39ff159b0d8b4ae090f2f2
mohd shahbaz mirza <officialshahbaz98@gmail.com>
2021-07-18 19:14:13 +0530

    matrix with row with maximum 1s in binary sorted matrix(row only)

---
+++ Matrix/matrixwithmax1s.java
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+        
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+        // code here
+        int ans=-1;
+        int j=0;
+        for(j=0;j<m;j++)
+        {
+            if(arr[0][j]==1)
+                {
+                    ans=0;
+                    break;
+                }
+        }
+        if(j==0)
+        return 0;
+        else
+        j=j-1;
+        int i=1;
+        while(i<n)
+        {
+            if(arr[i][j]==1)
+            {
+                ans=i;
+                if(j!=0)
+                    j--;
+                else
+                    return i;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return ans;
+    }
+}
