29a017e8b633ec4ecf925105b544cb1940253e97
mohd shahbaz mirza <86179579+mohdshahbazmirza@users.noreply.github.com>
2021-08-16 00:56:46 +0530

    925. Long Pressed Name leetcode

---
+++ String/Longpress.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i=0,j=0;
+        while(j<typed.length())
+        {
+            if(i<name.length() && name.charAt(i)==typed.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                if(j!=0 && name.charAt(i-1)==typed.charAt(j))
+                    j++;
+                else 
+                    return false;
+            }
+        }
+        return i==name.length();
+    }
+}
